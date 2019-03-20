7b449e5c1579ffa3ab00a02a1b938996d4deab7e
AlexBrandt46 <ajb3155@gmail.com>
2019-03-20 00:24:09 -0400

    Added variables to keep track of time

--- source.ino
+++ source.ino
@@ -1,7 +1,10 @@
 const unsigned short int btnPin = 12; //the pin of the pushbutton
 const short int btnState = 0; //keeps track of whether or not the button has been pushed
+int startPress = 0; //the time the button was pressed
+int endPress = 0; //the time the button was released
 
 void setup() {
+  Serial.begin(9600);
   pinMode(btnPin, INPUT); //marks the pin as an input
 }
 
@@ -11,6 +14,7 @@ void loop() {
 
   //checks if the button is being pushed or not
   if (buttonState == HIGH) {
+    startPressed = millis(); //assigns the time the button was pressed as the time since the program's started running
     
   }
   else {
